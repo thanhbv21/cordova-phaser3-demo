3c7cd8c1090f20a2f190632ea375fa257eb7eb00
thanh.bui <thanh.bui@niteco.se>
2019-05-24 11:06:05 +0700

    Update code

---
+++ js/index.js
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+// var app = {
+//     // Application Constructor
+//     initialize: function() {
+//         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+//     },
+
+//     // deviceready Event Handler
+//     //
+//     // Bind any cordova events here. Common events are:
+//     // 'pause', 'resume', etc.
+//     onDeviceReady: function() {
+//         this.receivedEvent('deviceready');
+//     },
+
+//     // Update DOM on a Received Event
+//     receivedEvent: function(id) {
+//         var parentElement = document.getElementById(id);
+//         var listeningElement = parentElement.querySelector('.listening');
+//         var receivedElement = parentElement.querySelector('.received');
+
+//         listeningElement.setAttribute('style', 'display:none;');
+//         receivedElement.setAttribute('style', 'display:block;');
+
+//         console.log('Received Event: ' + id);
+//     }
+// };
+
+// app.initialize();
+
+document.addEventListener('deviceready', function() {
+    var config = {
+        type: Phaser.WEBGL,
+        parent: 'game',
+        width: 800,
+        height: 480,
+        scene: {
+            preload: preload,
+            create: create,
+            update: update
+        }
+    };
+    
+    var game = new Phaser.Game(config);
+
+    function resize() {
+        var canvas = game.canvas, width = window.innerWidth, height = window.innerHeight;
+        var wratio = width / height, ratio = canvas.width / canvas.height;
+
+        if (wratio < ratio) {
+            canvas.style.width = width + "px";
+            canvas.style.height = (width / ratio) + "px";
+        } else {
+            canvas.style.width = (height * ratio) + "px";
+            canvas.style.height = height + "px";
+        }
+    }    
+    
+    function preload() {
+        this.load.atlas('sheet', 'img/sheet.png', 'img/sheet.json');
+    }
+    
+    function create() {
+        window.addEventListener('resize', resize);
+        resize();
+        
+        this.anims.create({
+            key: 'plane',
+            repeat: -1,
+            frameRate: 10,
+            frames: this.anims.generateFrameNames('sheet', { start: 1,  end: 3, prefix: 'planeBlue', suffix: '.png' })
+        });
+        this.bg = this.add.tileSprite(0, 0, 800, 480, 'sheet', 'background.png').setOrigin(0);
+        var plane = this.add.sprite(400, 300, 'sheet').play('plane');
+    }
+    
+    function update() {
+        this.bg.tilePositionX += 5;
+    }
+});
+
+if (!window.cordova) {
+    window.dispatchEvent('deviceready');
+}
